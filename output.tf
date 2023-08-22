7dac1889a107c4390275420a804308520917a18d
ar7u4 <109585047+ar7u4@users.noreply.github.com>
2023-08-22 21:46:07 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
-output "redshift_cluster_endpoint" {
-  value = aws_redshift_cluster.redshift_cluster.endpoint
-}
+#output "redshift_cluster_endpoint" {
+ # value = aws_redshift_cluster.redshift_cluster.endpoint
+#}
 
 output "generated_password" {
   value = random_password.password.result
