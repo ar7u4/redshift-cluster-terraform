b7af9c1207abbf2ef257ee158462a055cf8c6214
ar7u4 <109585047+ar7u4@users.noreply.github.com>
2023-08-23 11:42:57 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,1 @@
-variable "ssh_key_name" {
-  description = "Name for the SSH key pair"
-}
-
-variable "ssh_public_key" {
-  description = "SSH public key content"
-}
-
-variable "redshift_final_snapshot_identifier" {
-  description = "Final snapshot identifier for Redshift cluster"
-  default     = "my-final-snapshot"
-}
-
-variable "aws_region" {
-  description = "AWS region"
-  default     = "us-west-2"
-}
+Need to udpate vars
